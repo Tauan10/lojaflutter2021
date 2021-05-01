49a43bb32e7c2a6ef7bf040914a3e864449385ce
pedrohenrique <aguiarhora@gmail.com>
2021-05-01 17:01:24 -0400

    Bloqueando campos da senha com ChangeNotifier

--- lib/models/gerenciador_user.dart
+++ lib/models/gerenciador_user.dart
@@ -1,14 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:lojafinal/helpers/firebase_erros.dart';
 import 'package:lojafinal/models/user.dart';
 
 // USER MANAGER
 // validaçao do firebase email e senha
-class UserManager {
+class UserManager extends ChangeNotifier {
   final FirebaseAuth auth = FirebaseAuth.instance;
 
+  bool loading = false;
+
   Future<void> signIn({User user, Function onFail, Function onSuccess}) async {
+    setLoading(true);
+
     // ignore: unused_local_variable
     try {
       // ignore: unused_local_variable
@@ -19,5 +24,11 @@ class UserManager {
       // ignore: avoid_print
       onFail(getErrorString(e.code));
     }
+
+    setLoading(false);
+  }
+
+  void setLoading(bool value) {
+    loading = value;
   }
 }
--- lib/screens/login/login.screen.dart
+++ lib/screens/login/login.screen.dart
@@ -6,8 +6,8 @@ import 'package:provider/provider.dart';
 
 class LoginScreen extends StatelessWidget {
   final GlobalKey<FormState> formkey = GlobalKey<FormState>();
-  final GlobalKey<ScaffoldState> scaffoldMessengerKey =
-      GlobalKey<ScaffoldState>();
+  final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
+      GlobalKey<ScaffoldMessengerState>();
 
   final TextEditingController emailController = TextEditingController();
   final TextEditingController senhaController = TextEditingController();
@@ -27,38 +27,86 @@ class LoginScreen extends StatelessWidget {
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
           margin: const EdgeInsets.symmetric(horizontal: 16),
           child: Form(
-            key: formkey,
-            child: ListView(
-              padding: const EdgeInsets.all(16),
-              shrinkWrap: true,
-              children: [
-                TextFormField(
-                  controller: emailController,
-                  decoration: const InputDecoration(hintText: 'E-mail'),
-                  keyboardType: TextInputType.emailAddress,
-                  autocorrect: false,
-                  // ignore: missing_return
-                  validator: (email) {
-                    if (!emailValido(email)) return 'Email inválido';
-                    return null;
-                  },
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextFormField(
-                  controller: senhaController,
-                  decoration: const InputDecoration(hintText: 'Senha'),
-                  autocorrect: false,
-                  obscureText: true,
-                  validator: (senha) {
-                    if (senha.isEmpty || senha.length < 6)
-                      // ignore: curly_braces_in_flow_control_structures
-                      return 'Senha inválida';
-                    return null;
-                  },
-                ),
-                Align(
+              key: formkey,
+              child: Consumer<UserManager>(
+                builder: (_, userManager, child) {
+                  return ListView(
+                    padding: const EdgeInsets.all(16),
+                    shrinkWrap: true,
+                    children: [
+                      TextFormField(
+                        enabled: !userManager.loading,
+                        controller: emailController,
+                        decoration: const InputDecoration(hintText: 'E-mail'),
+                        keyboardType: TextInputType.emailAddress,
+                        autocorrect: false,
+                        // ignore: missing_return
+                        validator: (email) {
+                          if (!emailValido(email)) return 'Email inválido';
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      TextFormField(
+                        controller: senhaController,
+                        enabled: !userManager.loading,
+                        decoration: const InputDecoration(hintText: 'Senha'),
+                        autocorrect: false,
+                        obscureText: true,
+                        validator: (senha) {
+                          if (senha.isEmpty || senha.length < 6)
+                            // ignore: curly_braces_in_flow_control_structures
+                            return 'Senha inválida';
+                          return null;
+                        },
+                      ),
+                      child,
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      SizedBox(
+                        height: 44,
+                        child: ElevatedButton(
+                          // TODO: https://stackoverflow.com/questions/64995163/how-to-i-change-the-disabled-color-of-an-elevatedbutton
+                          onPressed: userManager.loading
+                              ? null
+                              : () {
+                                  if (formkey.currentState.validate()) {
+                                    userManager.signIn(
+                                        user: User(
+                                            email: emailController.text,
+                                            senha: senhaController.text),
+                                        //callback
+                                        onFail: (e) {
+                                          ScaffoldMessenger.of(context)
+                                              .showSnackBar(SnackBar(
+                                            content: Text('Falha ao entrar:$e'),
+                                          ));
+                                        },
+                                        onSuccess: () {
+                                          // TODO: FECHAR TELA DE LOGN
+                                        });
+                                  }
+                                },
+                          // ignore: prefer_const_constructors
+
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color.fromARGB(255, 235, 155, 0),
+                          ),
+
+                          child: const Text(
+                            'Entrar',
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
+                        ),
+                      )
+                    ],
+                    // primary: Color.fromARGB(255, 235, 155, 0*/
+                  );
+                },
+                child: Align(
                   alignment: Alignment.centerRight,
                   child: TextButton(
                       onPressed: () {},
@@ -67,43 +115,7 @@ class LoginScreen extends StatelessWidget {
                         style: TextStyle(color: Colors.black),
                       )),
                 ),
-                const SizedBox(
-                  height: 16,
-                ),
-                SizedBox(
-                  height: 44,
-                  child: ElevatedButton(
-                      // TODO: https://stackoverflow.com/questions/64995163/how-to-i-change-the-disabled-color-of-an-elevatedbutton
-                      onPressed: () {
-                        if (formkey.currentState.validate()) {
-                          context.read<UserManager>().signIn(
-                              user: User(
-                                  email: emailController.text,
-                                  senha: senhaController.text),
-                              //callback
-                              onFail: (e) {
-                                ScaffoldMessenger.of(context)
-                                    .showSnackBar(SnackBar(
-                                  content: Text('Falha ao entrar:$e'),
-                                ));
-                              },
-                              onSuccess: () {
-                                // TODO: FECHAR TELA DE LOGN
-                              });
-                        }
-                      },
-                      // ignore: prefer_const_constructors
-                      style: ElevatedButton.styleFrom(
-                          primary: const Color.fromARGB(255, 235, 155, 0)),
-                      child: const Text(
-                        'Entrar',
-                        style: TextStyle(color: Colors.white, fontSize: 18),
-                      )),
-                )
-              ],
-              // primary: Color.fromARGB(255, 235, 155, 0*/
-            ),
-          ),
+              )),
         ),
       ),
     );
