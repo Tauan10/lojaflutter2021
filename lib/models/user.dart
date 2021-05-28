9eeba0f0c48d616f12944d6a65eb7d9b95bf0256
Pedro Henrique <aguiarhora@gmail.com>
2021-05-27 21:56:12 -0400

    Salvando os dados do usuario no banco de dados

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,9 +1,28 @@
-class User {
-  User({this.email, this.senha, this.name});
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class User {
+  User({this.email, this.senha, this.name, this.id});
+  String id;
   String name;
   String email;
   String senha;
 
   String confirmacaoSenha;
-}
+
+  DocumentReference get firestoreRef => 
+  Firestore.instance.document('user/$id');
+
+
+  Future<void> saveData()async{
+  await firestoreRef.setData(toMap());
+  }
+
+// Aqui transforma os dados aqui do usuario em mapa
+    Map<String, dynamic> toMap(){
+      return{
+        'name': name,
+        'email':email,
+      };
+    }
+  }
+
