5bb4879ff659fb8d2135e6a3f9776d4c12c96564
Pedro Henrique <aguiarhora@gmail.com>
2021-06-03 14:27:52 -0400

    Alteraçao de multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lojafinal/models/gerenciador_produto.dart';
 import 'package:lojafinal/models/gerenciador_user.dart';
 import 'package:lojafinal/screens/base/base_screens.dart';
 import 'package:lojafinal/screens/login/cadastro/cadastro_screen.dart';
@@ -13,31 +14,39 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => UserManager(),
-      lazy: false,
-      child: MaterialApp(
-        title: 'Loja de Informatica',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            fontFamily: 'Comfortaa',
-            primaryColor: const Color.fromARGB(255, 235, 155, 0),
-            scaffoldBackgroundColor: const Color.fromARGB(2255, 235, 155, 0),
-            visualDensity: VisualDensity.adaptivePlatformDensity,
-            appBarTheme: const AppBarTheme(elevation: 0)),
-        initialRoute: '/base',
-        onGenerateRoute: (settings) {
-          switch (settings.name) {
-            case '/login':
-              return MaterialPageRoute(builder: (_) => LoginScreen());
-            case '/signup':
-              return MaterialPageRoute(builder: (_) => CadastroScreen());
-            case '/base':
-            default:
-              return MaterialPageRoute(builder: (_) => BaseScreen());
-          }
-        },
-      ),
+    return MultiProvider(
+          providers: [
+            ChangeNotifierProvider(
+              create: (_) => UserManager(),
+              lazy: false,
+              ),
+              ChangeNotifierProvider(
+              create: (_) => ProductManager(),
+              lazy: false,
+              )
+          ],
+          child: MaterialApp(
+          title: 'Loja de Informatica',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+      fontFamily: 'Comfortaa',
+      primaryColor: const Color.fromARGB(255, 235, 155, 0),
+      scaffoldBackgroundColor: const Color.fromARGB(2255, 235, 155, 0),
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+      appBarTheme: const AppBarTheme(elevation: 0)),
+          initialRoute: '/base',
+          onGenerateRoute: (settings) {
+            switch (settings.name) {
+      case '/login':
+        return MaterialPageRoute(builder: (_) => LoginScreen());
+      case '/signup':
+        return MaterialPageRoute(builder: (_) => CadastroScreen());
+      case '/base':
+      default:
+        return MaterialPageRoute(builder: (_) => BaseScreen());
+            }
+          },
+        ),
     );
   }
 }
