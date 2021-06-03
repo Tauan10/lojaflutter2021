0443fcc04091ce32bafaab8be042acaca3134d7a
Pedro Henrique <aguiarhora@gmail.com>
2021-06-03 10:16:09 -0400

    Colocando numero 1 em senha invalida

--- lib/screens/login/login.screen.dart
+++ lib/screens/login/login.screen.dart
@@ -73,7 +73,7 @@ class LoginScreen extends StatelessWidget {
                         validator: (senha) {
                           if (senha.isEmpty || senha.length < 6)
                             // ignore: curly_braces_in_flow_control_structures
-                            return 'Senha inválida';
+                            return 'Senha inválida 1';
                           return null;
                         },
                       ),
