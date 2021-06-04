e9e4e378d377193f9158f806ffec30db18e66f41
Pedro Henrique <aguiarhora@gmail.com>
2021-06-03 20:33:39 -0400

    Criaçao da tela do produto em exibição

--- lib/screens/login/produtos/produto_screen.dart
+++ lib/screens/login/produtos/produto_screen.dart
@@ -1,1 +1,30 @@
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
+import 'package:lojafinal/models/produtos.dart';
+
+
+class ProductScreen extends StatelessWidget {
+
+  const ProductScreen(this.product);
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.name),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: <Widget>[
+          Carousel(
+            images: product.images.map((url) {
+              return NetworkImage(url);
+            } ).toList(),
+          )
+        ],
+      ),
+    );
+  }
+}
